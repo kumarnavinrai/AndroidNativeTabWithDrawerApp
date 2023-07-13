9789e1b99bd3522e3db638a1892221dff07fa69d
Navin Kumar <kumarnavinrai@hotmail.com>
2023-07-13 22:15:58 +0800

    second commit

---
+++ app/src/main/java/com/example/mrtitanic/PlaceholderFragment.java
@@ -0,0 +1,27 @@
+package com.example.mrtitanic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mrtitanic.R;
+
+public class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_placeholder, container, false);
+        // Customize the fragment's layout and functionality here
+        return view;
+    }
+}
